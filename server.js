21f923693eef350aa00ceab4483672108d73665c
OraonShivam21 <oraonshivam.0302@gmail.com>
2023-12-13 14:13:53 +0530

    updated server.js

--- server.js
+++ server.js
@@ -20,6 +20,23 @@ app.get("/", (req, res) => {
   }
 });
 
+app.post("/login", (req, res) => {
+  try {
+    if (!req.body || !req.body.username || !req.body.password) {
+      res.status(400).send("Bad request, required username and password both");
+    }
+
+    const user = apiData.users.find((u) => u.username === req.body.username);
+    if (user == null) {
+      res.status(400).send({ error: `Cannot find user: ${req.body.username}` });
+    } else {
+      res.status(200).send({ response: "OK" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 app.get("/users", (req, res) => {
   try {
     res.send(apiData.users);
@@ -29,12 +46,27 @@ app.get("/users", (req, res) => {
   }
 });
 
+app.get("/users/:id", (req, res) => {
+  try {
+    const user = apiData.users.find((u) => u.userID === req.params.id);
+    if (user == null) {
+      res
+        .status(400)
+        .send({ error: `Cannot find user: ${req.params.username}` });
+    } else {
+      res.status(200).send(user);
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 app.get("/services", (req, res) => {
   try {
     res.send(apiData.services);
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ message: error.message });
+    res.status(500).send({ message: error.message });
   }
 });
 
@@ -43,7 +75,24 @@ app.get("/packages", (req, res) => {
     res.send(apiData.packages);
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ message: error.message });
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/packages/:packageID", (req, res) => {
+  try {
+    const package = apiData.packages.find(
+      (package) => package.packageID === req.params.packageID
+    );
+    if (package == null) {
+      res
+        .status(400)
+        .send({ error: `Cannot find package: ${req.params.packageID}` });
+    } else {
+      res.status(200).send(package);
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
 });
 
